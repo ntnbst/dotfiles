d5363cfd137350291a7bd90beb629ac44e926714
nitin <nitin@192.168.1.2>
2024-05-19 22:20:46 +0530

    add dot files

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+-- return {
+--   "craftzdog/solarized-osaka.nvim",
+--   lazy = true,
+--   priority = 1000,
+--   opts = function()
+--     return {
+--       transparent = true,
+--     }
+--   end,
+-- }
+
+return {
+  "folke/tokyonight.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return { transparent = true, style = "storm" }
+  end,
+}
+
+-- return {
+--   -- { "olivercederborg/poimandres.nvim" },
+--   { "shaunsingh/nord.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       -- colorscheme = "poimandres",
+--       -- style = "storm",
+--       colorscheme = "nord",
+--       transparent = true,
+--     },
+--   },
+-- }
+
+-- return { "rose-pine/neovim", name = "rose-pine" }
